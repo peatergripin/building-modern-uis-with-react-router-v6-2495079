8c18f780e5f74dfe5e0aae6fc0034d1dbbe3284e
Irfan bin Imran <peatergripin@gmail.com>
2026-01-14 16:31:02 +0800

    complete exercise and challenge

--- src/components/Confirmation.js
+++ src/components/Confirmation.js
@@ -1,7 +1,7 @@
 import { useLocation } from "react-router-dom";
 
 export default function Confirmation() {
-  const { state, name } = useLocation();
+  const { state } = useLocation();
   return (
     <div className="container">
       <h1>Thank You!</h1>
