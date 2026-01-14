12b7ecdedec17877189b1d0501d7a0b1bacfc9f7
Irfan bin Imran <peatergripin@gmail.com>
2026-01-14 16:27:53 +0800

    completed exercises

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,46 @@
+import { Routes, Route, Link } from "react-router-dom";
 import logo from "../assets/red30-tech-logo.png";
-
+import Home from "./Home.js";
+import About from "./About.js";
+import Categories from "./Categories.js";
+import Category from "./Category.js";
+import Header from "./Header.js";
+import Session from "./Session.js";
+import Register from "./Register.js";
+import Confirmation from "./Confirmation.js";
 function App() {
   return (
     <div className="app">
-      <header className="container">
+      {/* <header className="container">
         <img
           className="logo"
           src={logo}
           alt="Red30 Tech logo"
           title="Red30 Tech | Home"
         />
-      </header>
-
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/categories">Categories</Link>
+          <Link to="/about">About</Link>
+        </nav>
+      </header> */}
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home title="Welcome to Red30 Tech" />} />
+        <Route path="about" element={<About />} />
+        <Route path="categories" element={<Categories />}>
+          <Route path=":catId" element={<Category />}>
+            <Route path=":sessionId" element={<Session />} />
+          </Route>
+          <Route index element={<h3>Select a category from above</h3>} />
+        </Route>
+        <Route path="register" element={<Register />} />
+        <Route path="confirmed" element={<Confirmation />} />
+        <Route
+          path="*"
+          element={<h1 className="not-found">Page Not Found</h1>}
+        />
+      </Routes>
       <footer className="container">
         &copy;2022 | <a href="https://red30tech.com/">Red30 Tech</a>
       </footer>
--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,7 +1,25 @@
+import { getCategories } from "../api";
+import { Outlet, Link, NavLink } from "react-router-dom";
 export default function Categories() {
+  const categories = getCategories();
   return (
     <div className="container">
       <h1>Session Categories</h1>
+      <ul className="categories">
+        {categories.map((cat) => (
+          <li key={cat.id}>
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? "category-active" : null
+              }
+              to={cat.id}
+            >
+              {cat.name}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+      <Outlet />
     </div>
   );
 }
